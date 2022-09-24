b986a3521a7ef45903875cd5b2d4321cbea28534
karam <karammtr@gmail.com>
2022-09-24 21:21:39 +0200

    working css and display, need to get arthimatics to work

--- src/lib/component/Display.svelte
+++ src/lib/component/Display.svelte
@@ -1,6 +1,5 @@
 <script>
     import { counterValue } from "$lib/stores/counter.js";
-    import Key from "./Key.svelte";
 
     let lcd = null;
     let memory = 0;
--- src/lib/component/Key.svelte
+++ src/lib/component/Key.svelte
@@ -1,5 +1,4 @@
 <script>
-    import Display from "./Display.svelte";
     import { counterValue } from "$lib/stores/counter.js";
 
     function buttonClick(e) {
@@ -31,28 +30,101 @@
         lcd.value += digit; // adderar tal till lcd displayen, digit är siffran som finns efter bokstaven b   i idet på det valda knappen
         counterValue = lcd.value;
     }
+
+    /** Rensar display */
+    function clearLCD() {
+        lcd.value = "";
+        isComma = false;
+    }
+    /** Rensar allt, reset */
+    function memClear() {
+        memory = 0;
+        arithmetic = null;
+        clearLCD();
+    }
+
+    function setOperator(operator) {
+        switch (
+            operator // switch case , ifall .... då ...
+        ) {
+            case "+": // ifall man klicker på + då ...
+                memory = lcd.value; // memory får värdet av displayen
+                clearLCD(); // displayen clearas
+                arithmetic = "+"; //arithmetic får värdet av '+' // se funktion längre ner!
+                break;
+
+            case "-":
+                memory = lcd.value;
+                clearLCD();
+                arithmetic = "-";
+                break;
+
+            case "*":
+                memory = lcd.value;
+                clearLCD();
+                arithmetic = "*";
+                break;
+
+            case "/":
+                memory = lcd.value;
+                clearLCD();
+                arithmetic = "/";
+                break;
+        }
+    }
+    /**
+     * Beräknar ovh visar resultatet på displayen.
+     */
+    function calculate() {
+        switch (
+            arithmetic // swich case för arthimatic.
+        ) {
+            case "+": // om arthimatic = '+',   (se längre upp  där det uppreps)
+                result = Number(memory) + Number(lcd.value); // varaibelen resultat på värdet av lcd och det gamla värdet som finns i memory. Number() är för att kunna räkna med decimaltal, hade kunnat använt paresInt(), eller paresfloat()
+                lcd.value = result; // resultatet sätts i lcd displayen.
+                break;
+
+            case "-":
+                result = Number(memory) - Number(lcd.value);
+                lcd.value = result;
+                break;
+
+            case "*":
+                result = Number(lcd.value) * Number(memory);
+                lcd.value = result;
+                break;
+
+            case "/":
+                result = Number(memory) / Number(lcd.value);
+                lcd.value = result;
+                break;
+            default:
+        }
+    }
 </script>
 
 <section id="keyBoard">
     <button id="b7" on:click={buttonClick}>7</button>
     <button id="b8" on:click={buttonClick}>8</button>
     <button id="b9" on:click={buttonClick}>9</button>
-    <button id="add" on:click={buttonClick}>+</button>
+    <button id="add" on:click={setOperator}>+</button>
 
     <button id="b4" on:click={buttonClick}>4</button>
     <button id="b5" on:click={buttonClick}>5</button>
     <button id="b6" on:click={buttonClick}>6</button>
-    <button id="sub" on:click={buttonClick}>-</button>
+    <button id="sub" on:click={setOperator}>-</button>
 
-    <button id="b1"on:click={buttonClick}>1</button>
-    <button id="b2"on:click={buttonClick}>2</button>
-    <button id="b3"on:click={buttonClick}>3</button>
-    <button id="mul" on:click={buttonClick}>x</button>
+    <button id="b1" on:click={buttonClick}>1</button>
+    <button id="b2" on:click={buttonClick}>2</button>
+    <button id="b3" on:click={buttonClick}>3</button>
+    <button id="mul" on:click={setOperator}>x</button>
 
-    <button id="comma"on:click={buttonClick}>,</button>
-    <button id="b0"on:click={buttonClick}>0</button>
-    <button id="enter"on:click={buttonClick}>=</button>
-    <button id="div"on:click={buttonClick}>/</button>
+    <button id="comma" on:click={buttonClick}>,</button>
+    <button id="b0" on:click={buttonClick}>0</button>
+    <button id="enter" on:click={calculate}>=</button>
+    <button id="div" on:click={setOperator}>/</button>
 
-    <button on:click={buttonClick} id="clear" style="grid-column: span 4;">CLEAR</button>
+    <button on:click={clearLCD} on:dblclick={memClear} id="clear" style="grid-column: span 4;"
+        >CLEAR</button
+    >
 </section>
