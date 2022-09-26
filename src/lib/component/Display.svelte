be33060e78ed4b77621ed27807cf83aa27fdaf11
karam <karammtr@gmail.com>
2022-09-26 16:19:56 +0200

    code rewrite, MiniCalc v.2.0

--- src/lib/component/Display.svelte
+++ src/lib/component/Display.svelte
@@ -2,6 +2,4 @@
     import { counterValue } from "$lib/stores/counter.js";
 </script>
 
-<main>
     <input type="text" id="lcd" disabled="disabled" />
-</main>
--- src/lib/component/Key.svelte
+++ src/lib/component/Key.svelte
@@ -1,29 +1,29 @@
 <script>
     import Display from "./Display.svelte";
+    import { counterValue } from "$lib/stores/counter.js";
+
 </script>
 
-<main>
-    <section id="keyBoard">
-        <button id="b7">7</button>
-        <button id="b8">8</button>
-        <button id="b9">9</button>
-        <button id="add">+</button>
+<section id="keyBoard">
+    <button value ="7" id="b7" on:click>7</button>
+    <button value ="8"id="b8" on:click>8</button>
+    <button value ="9"id="b9" on:click>9</button>
+    <button value ="+" id="add" on:click>+</button>
 
-        <button id="b4">4</button>
-        <button id="b5">5</button>
-        <button id="b6">6</button>
-        <button id="sub">-</button>
+    <button value ="4" id="b4" on:click>4</button>
+    <button value ="5" id="b5" on:click>5</button>
+    <button value ="6" id="b6" on:click>6</button>
+    <button value ="-" id="sub" on:click>-</button>
 
-        <button id="b1">1</button>
-        <button id="b2">2</button>
-        <button id="b3">3</button>
-        <button id="mul">x</button>
+    <button value ="1" id="b1"on:click>1</button>
+    <button value ="2" id="b2"on:click>2</button>
+    <button value ="3" id="b3"on:click>3</button>
+    <button value ="x" id="mul" on:click>x</button>
 
-        <button id="comma">,</button>
-        <button id="b0">0</button>
-        <button id="enter">=</button>
-        <button id="div">/</button>
+    <button value ="." id="comma"on:click>,</button>
+    <button value ="0" id="b0"on:click>0</button>
+    <button value ="=" id="enter"on:click>=</button>
+    <button value ="/" id="div"on:click>/</button>
 
-        <button id="clear" style="grid-column: span 4;">CLEAR</button>
-    </section>
-</main>
+    <button value ="CL" id="clear" style="grid-column: span 4;">CLEAR</button>
+</section>
