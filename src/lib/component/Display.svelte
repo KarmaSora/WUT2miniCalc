3a6ce6b8c1253381604ec41441deadf79d70e986
karam <karammtr@gmail.com>
2022-09-26 14:38:08 +0200

    working display

--- src/lib/component/Display.svelte
+++ src/lib/component/Display.svelte
@@ -1,23 +1,14 @@
 <script>
-  let display = "";
-  
-   function updateDisplay(e){
-       let val = e.target.value; // Knappens value-värde
-       if(val === "ac"){
-           display = "";
-       }else if(val === "c"){
-           display = display.slice(0, -1);
-       }else if(Number(val) >= 0 && Number(val) <10 ){
-           display += val;
-       }
-    }
+    import { counterValue } from "$lib/stores/counter.js";
+    import Key from "./Key.svelte";
 
+    let lcd = null;
+    let memory = 0;
+    $: memory = lcd;
+    console.log(memory);
 
 
 </script>
 
-<fieldset>
-    <input type="text" value={display} disabled />
-    <keyBoard on:click = {updateDisplay}/>
- </fieldset>
- 
+
+    <input type="text" id="lcd" disabled="disabled" />
