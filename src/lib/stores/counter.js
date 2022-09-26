664dd8cd6182f666ecbc53373b95e4483a6c857e
karam <karammtr@gmail.com>
2022-09-26 10:43:39 +0200

    tappade koll på koden, ska byta tillbaka till gammal kod

--- src/lib/stores/counter.js
+++ src/lib/stores/counter.js
@@ -1,4 +1,6 @@
 import { writable } from "svelte/store";
  
 export const counterValue = writable(0);
-export const MemH = writable();
+console.log({counterValue});
+export const MemH = writable(0);
+console.log({MemH});
