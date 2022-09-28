018dba45f9796943e027ee47b511d04f65eed269
karam <karammtr@gmail.com>
2022-09-28 14:57:23 +0200

    samma kod som i master

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -3,45 +3,36 @@
     import Display from "$lib/component/Display.svelte";
     import Key from "$lib/component/Key.svelte";
     import { counterValue } from "$lib/stores/counter.js";
-  
+
     let arithmetic = null;
-    let memory = 0;
-    let lcd = "5";
-   
-      
+    let memory = "";
+    let lcd = "";
+    let result = "";
+
+    $: console.log({lcd});
+    $: console.log({result}) ;      
     function buttonClick(e) {
         let btn = e.target.value; //id för den tangent som tryckte ner
         // kollar om siffertangent är nedtryckt
-        if(Number(btn) >= 0 && Number(btn) <10 ){
+        console.log({btn})
+        if( btn === "+"){
+            memory = lcd; // memory får värdet av displayen
+              
+                              
+                result = Number(memory) + Number(lcd); // varaibelen resultat på värdet av lcd och det gamla värdet som finns i memory. Number() är för att kunna räkna med decimaltal, hade kunnat använt paresInt(), eller paresfloat()
+                lcd = result; // resultatet sätts i lcd displayen.
+                
+
+        }
+
+        else if(Number(btn) >= 0 && Number(btn) <10 ){
            lcd += btn;
         }
-  
-  
-  
-        /*
-                                                    Gammal Kod
-        if (btn.substring(0, 1) === "b") {
-            let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-            lcd += digit; // skriver ut digit/ tal  genom att plocka talen från knabbar vas id har bokstaven b i form av en string
-        } else {
-            // Inte en siffertangent, övriga tangenter.
-            if (btn.substring(0, 1) === "a") {
-                // kontrolerar om idet på knappen som trycks börjar med en viss bokstav, och sedan kör en funktion
-                setOperator("+"); // kör funktionen setOperator('+')
-            }
-            if (btn.substring(0, 1) === "s") {
-                setOperator("-");
-            }
-            if (btn.substring(0, 1) === "m") {
-                setOperator("*");
-            }
-            if (btn.substring(0, 1) === "d") {
-                setOperator("/");
-            }
-        }*/
-     
+
+
+      
     }
-  
+
     function setOperator(operator) {
         switch (
             operator // switch case , ifall .... då ...
@@ -51,19 +42,19 @@
                 clearLCD(); // displayen clearas
                 arithmetic = "+"; //arithmetic får värdet av '+' // se funktion längre ner!
                 break;
-  
+
             case "-":
                 memory = lcd;
                 clearLCD();
                 arithmetic = "-";
                 break;
-  
+
             case "*":
                 memory = lcd;
                 clearLCD();
                 arithmetic = "*";
                 break;
-  
+
             case "/":
                 memory = lcd;
                 clearLCD();
@@ -82,17 +73,17 @@
                 result = Number(memory) + Number(lcd); // varaibelen resultat på värdet av lcd och det gamla värdet som finns i memory. Number() är för att kunna räkna med decimaltal, hade kunnat använt paresInt(), eller paresfloat()
                 lcd= result; // resultatet sätts i lcd displayen.
                 break;
-  
+
             case "-":
                 result = Number(memory) - Number(lcd);
                 lcd = result;
                 break;
-  
+
             case "*":
                 result = Number(lcd) * Number(memory);
                 lcd = result;
                 break;
-  
+
             case "/":
                 result = Number(memory) / Number(lcd);
                 lcd = result;
@@ -100,15 +91,15 @@
             default:
         }
     }
-  
+
     function addComma() {
         // en funktion för att tilläga  ett kommatecken till lcd displayen
         lcd.value += ".";
     }
-  
+
     /** Rensar display */
     function clearLCD() {
-        lcd.value = "";
+        lcd = "";
     }
     /** Rensar allt, reset */
     function memClear() {
@@ -116,9 +107,9 @@
         arithmetic = null;
         clearLCD();
     }
- </script>
-  
- <html lang="en">
+</script>
+
+<html lang="en">
     <head>
         <title> SvCalc</title>
         <meta charset="UTF-8" />
@@ -126,7 +117,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="stylesheet"  href="/home/karam/Skrivbord/WUT/Te3b-WUT2/miniCalc/src/lib/global.css" />
     </head>
-  
+
     <body>
         <main>
             <article>
@@ -135,9 +126,4 @@
             </article>
         </main>
     </body>
- </html>
-  
-    
- 
- 
- 
+</html>
