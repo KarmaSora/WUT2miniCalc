72782ec91e29bbaf4bfc7f7e3ff3751a2b5274a7
karam <karammtr@gmail.com>
2022-09-28 13:10:41 +0200

    one step closer, funktioner fungerar, nästan

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -5,40 +5,32 @@
     import { counterValue } from "$lib/stores/counter.js";
 
     let arithmetic = null;
-    let memory = 0;
-    let lcd = "5";
+    let memory = "";
+    let lcd = "";
+    let result = "";
+
+    $: console.log({lcd})
     
        
     function buttonClick(e) {
         let btn = e.target.value; //id för den tangent som tryckte ner
         // kollar om siffertangent är nedtryckt
-        if(Number(btn) >= 0 && Number(btn) <10 ){
+        console.log({btn})
+        if( btn === "+"){
+            memory = lcd; // memory får värdet av displayen
+              
+                              
+                result = Number(memory) + Number(lcd); // varaibelen resultat på värdet av lcd och det gamla värdet som finns i memory. Number() är för att kunna räkna med decimaltal, hade kunnat använt paresInt(), eller paresfloat()
+                lcd = result; // resultatet sätts i lcd displayen.
+                
+
+        }
+
+        else if(Number(btn) >= 0 && Number(btn) <10 ){
            lcd += btn;
         }
 
 
-
-        /* 
-                                                    Gammal Kod
-        if (btn.substring(0, 1) === "b") {
-            let digit = btn.substring(1, 2); // plockar ut siffran från id:et
-            lcd += digit; // skriver ut digit/ tal  genom att plocka talen från knabbar vas id har bokstaven b i form av en string
-        } else {
-            // Inte en siffertangent, övriga tangenter.
-            if (btn.substring(0, 1) === "a") {
-                // kontrolerar om idet på knappen som trycks börjar med en viss bokstav, och sedan kör en funktion
-                setOperator("+"); // kör funktionen setOperator('+')
-            }
-            if (btn.substring(0, 1) === "s") {
-                setOperator("-");
-            }
-            if (btn.substring(0, 1) === "m") {
-                setOperator("*");
-            }
-            if (btn.substring(0, 1) === "d") {
-                setOperator("/");
-            }
-        }*/
       
     }
 
@@ -108,7 +100,7 @@
 
     /** Rensar display */
     function clearLCD() {
-        lcd.value = "";
+        lcd = "";
     }
     /** Rensar allt, reset */
     function memClear() {
